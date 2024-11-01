3c55d25310bf23e4a0d3a6b246c219e31f678adb
pumpkin99111 <pumpkin99111@gmail.com>
2024-11-01 11:27:29 +0800

    vr

---
+++ vr/app-files/data.js
@@ -0,0 +1,587 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "一號入口石碑",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.2177117484831683,
+        "pitch": 0.06499430392795347,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3777602338131718,
+          "pitch": 0.06076374402637974,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -1.2845512940541326,
+          "pitch": 0.012959777920354298,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "觀世音圖薩",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.9964302478008396,
+        "pitch": -0.20773888505667948,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4796447973587483,
+          "pitch": 0.07538660410090614,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "捷運站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.4892060413980097,
+        "pitch": 0.21717376581493397,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1402198510677266,
+          "pitch": 0.060008576676473524,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 1.0111539521434416,
+          "pitch": -0.1980525179489554,
+          "rotation": 0,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-1",
+      "name": "生態池1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8542754278891636,
+          "pitch": -0.046763122279905645,
+          "rotation": 0,
+          "target": "5-2"
+        },
+        {
+          "yaw": 3.040118305955451,
+          "pitch": 0.1634441261119033,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.6752716417607978,
+          "pitch": -0.07365688836178741,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": 0.30048347365151074,
+          "pitch": -0.07681849458679224,
+          "rotation": 0,
+          "target": "9-"
+        },
+        {
+          "yaw": 2.1464333877415616,
+          "pitch": 0.040794807222559015,
+          "rotation": 0,
+          "target": "10-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "岔路口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.645526466777964,
+          "pitch": 0.0537529793456919,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.8771169730817459,
+          "pitch": -0.0753587830503939,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -2.5514495603602807,
+          "pitch": 0.17754641941761662,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -1.0839990226175225,
+          "pitch": -0.03320632454401462,
+          "rotation": 0,
+          "target": "3-1"
+        },
+        {
+          "yaw": -1.6741863077135388,
+          "pitch": 0.10146164542360836,
+          "rotation": 0,
+          "target": "11-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-2",
+      "name": "生態池2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7156851333742917,
+          "pitch": 0.08492653486040425,
+          "rotation": 0,
+          "target": "3-1"
+        },
+        {
+          "yaw": 0.444789366588223,
+          "pitch": -0.09380986173789196,
+          "rotation": 0,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-",
+      "name": "遊樂場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.054168380139723,
+          "pitch": 0.09654014359137086,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.8104924188405747,
+          "pitch": -0.012923672697418453,
+          "rotation": 0,
+          "target": "5-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "音樂台",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9460931325578787,
+          "pitch": 0.171128467222875,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-",
+      "name": "樂活水利飛輪",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8882105989241547,
+          "pitch": 0.16787821129488023,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": -2.9167876887092117,
+          "pitch": 0.27473717362481054,
+          "rotation": 0,
+          "target": "9-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-",
+      "name": "老榕樹",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0114025609639095,
+          "pitch": 0.1364221858702379,
+          "rotation": 0,
+          "target": "3-1"
+        },
+        {
+          "yaw": -0.897663337808055,
+          "pitch": 0.05071500087473346,
+          "rotation": 0,
+          "target": "8-"
+        },
+        {
+          "yaw": 0.22930928991410227,
+          "pitch": -0.02574589031831742,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-",
+      "name": "廁所",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.170314532639111,
+          "pitch": 0.13255682474723507,
+          "rotation": 0,
+          "target": "3-1"
+        },
+        {
+          "yaw": 0.3601216014585731,
+          "pitch": -0.0452274627934095,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": -0.3148205130067545,
+          "pitch": 0.06765801324378273,
+          "rotation": 0,
+          "target": "11-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-",
+      "name": "籃球場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0163141560630145,
+          "pitch": 0.016870919694142117,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 2.15820381730466,
+          "pitch": 0.07300709010974771,
+          "rotation": 0,
+          "target": "3-1"
+        },
+        {
+          "yaw": 0.06191204618029822,
+          "pitch": -0.08476814666593313,
+          "rotation": 0,
+          "target": "5-2"
+        },
+        {
+          "yaw": 3.042887246115977,
+          "pitch": 0.14581103499733317,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
