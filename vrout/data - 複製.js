63b4c0841efeaf284a045c31d1ddcaaac614715d
pumpkin99111 <pumpkin99111@gmail.com>
2024-12-13 11:48:03 +0800

    Create data - 複製.js

---
+++ vrout/data - 複製.js
@@ -0,0 +1,265 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-book",
+      "name": "圖書館",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+	  {
+          "yaw": 0.41845426472969294,
+          "pitch": -0.03077863335549047,
+          "rotation": 0,
+          "target": "4-out5"
+        },
+        {
+          "yaw": 2.639236548335301,
+          "pitch": 0.0919603990126312,
+          "rotation": 0,
+          "target": "3-out3"
+        }],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-out1",
+      "name": "出口(信義新生路口)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1499729328622923,
+          "pitch": -0.031089953501409084,
+          "rotation": 0,
+          "target": "2-out2"
+        },
+        {
+          "yaw": 0.09514333602275471,
+          "pitch": -0.09096742348575226,
+          "rotation": 0,
+          "target": "5-out6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-out2",
+      "name": "捷運站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0864257722946853,
+          "pitch": 0.09497062543217183,
+          "rotation": 0,
+          "target": "1-out1"
+        },
+        {
+          "yaw": 0.054629879173212714,
+          "pitch": 0.06981322281940727,
+          "rotation": 0,
+          "target": "3-out3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-out3",
+      "name": "出口(朝向建國花市)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1085872955926028,
+          "pitch": 0.1886143518012915,
+          "rotation": 0,
+          "target": "2-out2"
+        },
+        {
+          "yaw": -3.062481520855222,
+          "pitch": 0.0957434941291968,
+          "rotation": 0,
+          "target": "0-book"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-out5",
+      "name": "出口(建國和平路口)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6867821016458269,
+          "pitch": 0.010170533211082855,
+          "rotation": 0,
+          "target": "0-book"
+        },
+        {
+          "yaw": -0.017740908708592684,
+          "pitch": -0.027683356248470403,
+          "rotation": 0,
+          "target": "5-out6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-out6",
+      "name": "出口(新生和平路口)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
